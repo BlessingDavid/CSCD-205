31a44faa9ba2b730d73c5895eee47db9677c22dc
BlessingDavid <42800923+BlessingDavid@users.noreply.github.com>
2018-08-30 00:39:26 +0000

    Update and rename README.md to a2_10662377.cc

---
+++ a2_10662377.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+  using namespace std;
+  bool is_prme (int num {
+  int f_count = 0;
+  for (int i = 1; i <=num; i++) {
+  if (num % i == 0) {
+  f_ count++;
+      } 
+}
+if (f_count == 2)  {
+        return true;
+    } else {
+        return false:
+    }
+}
+                          
